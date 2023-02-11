526d0e6593026928ffeeabfb52173ecb99a96868
ifnesi <inesi@confluent.io>
2023-02-11 17:16:33 +0000

    Set start order

--- start_demo.sh
+++ start_demo.sh
@@ -5,9 +5,9 @@ source _venv/bin/activate
 ./stop_demo.sh
 
 python3 svc_status.py $1 &
-python3 svc_delivery.py $1 &
-python3 svc_bake.py $1 &
 python3 svc_assemble.py $1 &
+python3 svc_bake.py $1 &
+python3 svc_delivery.py $1 &
 python3 app.py $1 &
 
 deactivate
