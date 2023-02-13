822ec24dabe97a0ea8459dff3e429a859dded684
ifnesi <inesi@confluent.io>
2023-02-13 12:28:25 +0000

    Dynamic alert colour

--- static/js/ajax_order.js
+++ static/js/ajax_order.js
@@ -1,6 +1,18 @@
 var order_id;
 var last_result;
 
+$(document).ready(function () {
+    order_id = $("#order_id").val();
+    setTimeout(function () {
+        update_order_status();
+    }, 1000);
+});
+
+function toggle_status(remove, add) {
+    $("#order_status").removeClass(remove);
+    $("#order_status").addClass(add);
+}
+
 // Update view order every 1 sec (in a realistic scenario that would be better off using REACT)
 function update_order_status() {
     if (order_id) {
@@ -11,22 +23,24 @@ function update_order_status() {
             dataType: "json",
             success: function (data) {
                 if (data) {
-                    if (last_result != 400) {
-                        last_result = data.status;
+                    if (data.status != 400) {
                         $("#order_status").text(data.str);
                         setTimeout(function () {
                             update_order_status();
                         }, 1000);
                     }
+                    if (data.status == 400) {
+                        toggle_status("badge-info", "badge-success");
+                    }
+                    else if (last_result != data.status) {
+                        toggle_status("badge-info", "badge-warning");
+                        setTimeout(function () {
+                            toggle_status("badge-warning", "badge-info");
+                        }, 1000);
+                    }
+                    last_result = data.status;
                 }
             }
         });
     }
 }
-
-$(document).ready(function () {
-    order_id = $("#order_id").val();
-    setTimeout(function () {
-        update_order_status();
-    }, 1000);
-});
