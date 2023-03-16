fb6be8a657bc9bcc6bf64eee27f7f59a9df81b5f
ifnesi <inesi@confluent.io>
2023-03-16 10:18:59 +0000

    Blink 3x times

--- static/js/ajax_order.js
+++ static/js/ajax_order.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     status_delivered = $("#status_delivered").val();
     setTimeout(function () {
         update_order_status();
-    }, 1000);
+    }, 3000);
 });
 
 function toggle_status(remove, add) {
@@ -29,16 +29,22 @@ function update_order_status() {
                         $("#order_status").text(data.str);
                         setTimeout(function () {
                             update_order_status();
-                        }, 1000);
+                        }, 3000);
                     }
                     if (data.status == status_delivered) {
                         toggle_status("badge-info", "badge-success");
                     }
                     else if (last_result != data.status && last_result != -1) {
-                        toggle_status("badge-info", "badge-warning");
-                        setTimeout(function () {
-                            toggle_status("badge-warning", "badge-info");
-                        }, 1000);
+                        var timeout;
+                        for (var i = 0; i < 3; i++) {
+                            timeout = i * 500;
+                            setTimeout(function () {
+                                toggle_status("badge-info", "badge-warning");
+                            }, timeout);
+                            setTimeout(function () {
+                                toggle_status("badge-warning", "badge-info");
+                            }, 250 + timeout);
+                        }
                     }
                     last_result = data.status;
                 }
